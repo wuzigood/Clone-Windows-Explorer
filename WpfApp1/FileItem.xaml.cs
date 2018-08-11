3bf5947c6852eb5c30a78722a8a1f7d2d8bf07a0
zhouzekai <zzk9460@gmail.com>
2018-08-11 11:15:49 +0800

    Back Up for copy and paste

---
+++ WpfApp1/FileItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// FileItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class FileItem : UserControl
+    {
+        public FileItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -22,55 +22,77 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         [DllImport("C:\\Users\\zzk\\source\\repos\\WpfApp1\\Debug\\core.dll", EntryPoint ="Sum", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Sum(int a, ref int b);
+        private static extern int Sum(int a, int b);
 
-        int count = 0;
+        [DllImport("C:\\Users\\zzk\\source\\repos\\WpfApp1\\Debug\\core.dll", EntryPoint = "GetCurrentDir", CharSet =CharSet.Unicode,CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr GetCurrentDir();
+
+        private int width = 100;
+        private int height = 100;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            IntPtr intPtr = GetCurrentDir();
+            string currentDir = Marshal.PtrToStringUni(intPtr);
+            AddressBar.Text = currentDir;
         }
 
         private void menuItem_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("menuItem Click");
+            MenuItem menuItem = (MenuItem)sender;
+            string val = menuItem.Header as string;
+            switch(val)
+            {
+                case "新建文件夹":
+                    Add_Folder();
+                    break;
+                case "新建文件":
+                    Add_File();
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {            
-            //MessageBox.Show(String.Format("{0} x {1}", MyGrid.ActualWidth, MyGrid.ActualHeight));
-            //ListBoxItem listBoxItem = new ListBoxItem();
-            ////listBox1.
-
-            //int b = 20;
-            //int a = Sum(10, ref b);
-            //label1.Content = b.ToString();
-            //count++;
-            //if (count > 4)
-            //{
-            //    MyGrid.RowDefinitions.Add(new RowDefinition());
-            //}
-            //Button button = new Button
-            //{
-            //    Name = "button" + count.ToString(),
-            //    Visibility = Visibility.Visible,
-            //    Content = count.ToString(),
-            //    Width = 200,
-            //    Height = 200
-            //};
-            //button.SetValue(Grid.ColumnProperty, 0);
-            //button.SetValue(Grid.RowProperty, 0);
-            //MyGrid.Children.Add(button);
+        {
         }
 
-        private void ListBox_DoubleClick(object sender, SelectionChangedEventArgs e)
+        private void Mouse_Enter()
         {
-            //label1.Content = "double click";
+
         }
 
-        private void CommomClick(object sender, RoutedEventArgs e)
+        private void Add_Folder()
         {
-            MessageBox.Show("Click");
+            Button button = new Button();
+            button.Width = width;
+            button.Height = height;
+            button.Visibility = Visibility.Visible;
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource =
+                new BitmapImage(
+                    new Uri("C:\\Users\\zzk\\source\\repos\\WpfApp1\\WpfApp1\\bin\\Debug\\folder.png")
+                );
+            button.Background = imageBrush;
+            wrapPanel.Children.Add(button);
+        }
+
+        private void Add_File()
+        {
+            Button button = new Button();
+            button.Width = width;
+            button.Height = height;
+            button.Visibility = Visibility.Visible;
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource =
+                new BitmapImage(
+                    new Uri("C:\\Users\\zzk\\source\\repos\\WpfApp1\\WpfApp1\\bin\\Debug\\document.ico")
+                );
+            button.Background = imageBrush;
+            wrapPanel.Children.Add(button);
         }
     }
 }
